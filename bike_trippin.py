e47100aa2957f6eb7ced8d0671f6fccd22583d81
piruzalemi <52976178+piruzalemi@users.noreply.github.com>
2019-12-02 19:16:55 -0500

    Create bike_trippin.py

---
+++ bike_trippin.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import Dependencies
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+# Import our data into pandas from CSV
+string_thing = '../Resources/trip.csv'
+bike_trips_df = pd.read_csv(string_thing, low_memory=False)
+
+bike_trips_df
+
+
+# In[3]:
+
+
+# Split up our data into groups based upon 'gender'
+gender_groups = bike_trips_df.groupby('gender')
+
+# Find out how many of each gender took bike trips
+gender_trips = gender_groups['tripduration'].count()
+
+
+# In[4]:
+
+
+gender_trips.head()
+
+
+# In[5]:
+
+
+# Drop the 'stoptime' row that is contained within our group
+gender_trips = gender_trips.drop("stoptime")
+
+# Chart our data, give it a title, and label the axes
+gender_chart = gender_trips.plot(kind="bar", title="Bike Trips by Gender")
+gender_chart.set_xlabel("Gender")
+gender_chart.set_ylabel("Number of Trips Taken")
+
+plt.show()
+plt.tight_layout()
+
+
+# In[5]:
+
+
+# Split up our data into groups based upon 'bikeid' and 'gender'
+bike_groups = bike_trips_df.groupby(['bikeid','gender'])
+
+# Create a new variable that holds the sum of our groups
+sum_it_up = bike_groups.sum()
+sum_it_up.head(12)
+
+
+# In[6]:
+
+
+# Make a variable called bike_id and store a 'bikeid' in it
+bike_id = "SEA00001"
+
+# Collect the trips of the 'bikeid' above
+just_one_bike = sum_it_up.loc[bike_id]
+
+# Place the gender keys for that single bike into a list
+gender_list = just_one_bike.keys()
+
+# Create a pie chart based upon the trip duration of that single bike
+bike_pie = just_one_bike.plot(kind="pie", y=gender_list, title=("Trips of " + bike_id))
+bike_pie.set_ylabel("Trip Duration")
+
+plt.show()
+plt.tight_layout()
+plt.axis("equal")
+
+
+# In[ ]:
+
+
+
+
